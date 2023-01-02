073fb9db3da1465f1df39badef3e9a046a1f473f
rafael seixas <rafael.s.costa96@gmail.com>
2023-01-02 18:37:22 +0000

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Content guide</h1>
+
+<div id="drawer-menu">
+    <a href="/event-timers">Event timers</a>
+    Fractals
+    Raids
+    Strike Missions
+    Account unlocks
+</div>
---
+++ src/routes/event-timers/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Event Timers</h1>
+
+<div>
+    <button>Central Tyria</button>
+    <button>Maguuma Jungle</button>
+    <button>Elona</button>
+    <button>Cantha</button>
+</div>
+<div>
+    PlaceHolder, fetch timers from API
+</div>
